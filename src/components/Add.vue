7a94820dd92e2a8c2c09112e3268096f1f064559
dsjerry <dsjerry@qq.com>
2020-07-18 11:38:14 +0800

    初始化vuex

---
+++ src/components/Add.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>当前最新的count：</h3>
+    <button>加一</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Sub.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>当前最新的count：</h3>
+    <button>减一</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
